15ab96c334d61957f7525b94aa521c7e8aa03d0d
md muhaiminul <muhai4959@gmail.com>
2023-03-30 22:18:25 +0600

    add header and fake data

---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            <div className='flex justify-between'>
+                <h1 className='text-3xl font-bold'>Knowledge Cafe</h1>
+                <img src="/public/images/admin.png" alt="admin" className='w-12' />
+            </div>
+            <hr className='border mt-2' />
+        </div>
+        
+    );
+};
+
+export default Header;
