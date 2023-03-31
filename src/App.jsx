5c2b0d37ec54238e37ec06dc3bb98dc7d98c5cd1
md muhaiminul <muhai4959@gmail.com>
2023-03-31 18:50:17 +0600

    modify header and add back to top feature

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,16 @@ import Header from './components/Header';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import HWBlog from './components/HWBlog';
+import { faArrowUp } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const App = () => {
   return (
-    <div className='mt-5 md:px-28 px-5 mb-10'>
+    <div className='mt-5 md:px-28 px-5 mb-10 relative'>
       <Header></Header>
       <BlogContainer></BlogContainer>
       <HWBlog></HWBlog>
+      <a href="#home" class="hidden md:block fixed z-90 bottom-8 right-8 border-0 px-5 py-3 rounded-full drop-shadow-md bg-indigo-500 text-white text-3xl font-bold"><FontAwesomeIcon icon={faArrowUp}></FontAwesomeIcon></a>
       <ToastContainer></ToastContainer>
     </div>
   );
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,9 +3,12 @@ import React from 'react';
 const Header = () => {
     return (
         <div>
-            <div className='flex justify-between'>
+            <div className='flex justify-between' id='home'>
                 <h1 className='text-3xl font-bold'>Aargon's Blog</h1>
-                <img src="/images/admin.png" alt="admin" className='w-12' />
+                <div className='flex items-center'>
+                    <a href="#react" className='text-2xl text-indigo-500 font-bold hover:text-amber-700 mx-5 md:me-20'>React</a>
+                    <img src="/images/admin.png" alt="admin" className='w-12' />
+                </div>
             </div>
             <hr className='border mt-2' />
         </div>
